ef000cdee13da5bd65048115010d0f9e9260765d
Soumyadeep Ghosh <92305008+KillerMax4299@users.noreply.github.com>
2023-03-09 19:11:28 +0530

    update

--- code.js
+++ code.js
@@ -1,8 +1,13 @@
 
-var cm;var inch=0;var foot=0;
+var cm;var inch;var foot;
 
+function clear_text(){
+    document.getElementById('ft').value = "";
+      document.getElementById('inch').value = "";
+      document.getElementById('cm').value = "";
+  }
 
-function rest() {
+function cm_to_im() {
   cm = document.getElementById('cm').value ;
     foot = 0;
     inch = cm/2.54;
@@ -12,9 +17,6 @@ function rest() {
       inch =  inch%12;
       inch = Number(inch.toFixed(0));
     }
-    
-    
-    
     document.getElementById('ft').value = foot;
     document.getElementById('inch').value = inch;
     
@@ -22,13 +24,74 @@ function rest() {
       document.getElementById('ft').value = "";
     if(inch == 0)
       document.getElementById('inch').value = "";
-
 };
-document.addEventListener('keyup', function(e) {rest()
-if(e.key ==='Delete'){
-  document.getElementById('ft').value = "";
-    document.getElementById('inch').value = "";
-    document.getElementById('cm').value = "";
+function in_to_cm(){
+  inch = document.getElementById('inch').value;
+  foot = document.getElementById('ft').value;
+
+  cm = Math.round(2.54*(12*Number(foot) +Number(inch))) ;
+  
+  document.getElementById('cm').value = cm;
+  if(cm ==0) 
+      document.getElementById('cm').value = "";
+      
 }
+
+function moving(){
+  if(document.activeElement === document.getElementById('ft'))
+    document.getElementById('inch').focus();
+  else
+    document.getElementById('ft').focus();
+}
+document.addEventListener('keydown', function(e) {
+  if(e.key === 'ArrowRight' || e.key === 'ArrowLeft'){
+    moving();
+  }
+  if(e.key === 'ArrowUp' || e.key === 'ArrowDown'){
+    if(!document.getElementById('cm').disabled){
+      document.getElementById('ft').disabled = false;
+      document.getElementById('inch').disabled = false;
+      document.getElementById('cm').disabled = true;
+      document.getElementById('ft').focus();
+    }
+    else{
+      document.getElementById('ft').disabled = true;
+      document.getElementById('inch').disabled = true;
+      document.getElementById('cm').disabled = false;
+      document.getElementById('cm').focus();
+    }
+  }
 },true);
-document.addEventListener('keydown', function(e) {rest()},true);
+
+function check(){
+  if(document.getElementById('inch').disabled && document.getElementById('ft').disabled)
+    cm_to_im();
+  else{
+    in_to_cm();
+  }
+}
+
+    document.addEventListener('keyup', function(e) {
+      check();
+      if(e.key ==='Delete'){
+        clear_text();
+      }},true);
+
+
+    
+
+
+document.getElementById('change').onclick = function() {
+  
+  if(!document.getElementById('cm').disabled){
+    document.getElementById('ft').disabled = false;
+    document.getElementById('inch').disabled = false;
+    document.getElementById('cm').disabled = true;
+  }
+  else{
+    document.getElementById('ft').disabled = true;
+    document.getElementById('inch').disabled = true;
+    document.getElementById('cm').disabled = false;
+  }
+ 
+}
